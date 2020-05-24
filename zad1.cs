c031846923ce2470cddff519f868904a2397c823
Ivan Jurisic <ivan.1jurisic@gmail.com>
2020-05-24 21:16:47 +0200

    Create zad1.cs

---
+++ zad1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lv1_RPPOON
+{
+    class zad1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
