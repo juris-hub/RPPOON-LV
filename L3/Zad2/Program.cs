9f5e4b248fb9a2958fd36fe1d005fe55a5b2a969
juris-hub <65715599+juris-hub@users.noreply.github.com>
2020-05-25 20:23:51 +0200

    Add files via upload

---
+++ L3/Zad2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] matrica = new double[5][];
+            for (int i = 0; i < 5; i++)
+            {
+                matrica[i] = new double[3];
+            }
+            RandomGenerator matrixgen = RandomGenerator.GetInstance();
+            matrica = matrixgen.NextMatrix(5, 5);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(matrica[i][j].ToString() + '\t');
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
---
+++ L3/Zad2/RandomGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3
+{
+    class RandomGenerator
+    {
+
+        private static RandomGenerator instance;
+        private Random generator;
+        private RandomGenerator()
+        {
+            this.generator = new Random();
+        }
+        public static RandomGenerator GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new RandomGenerator();
+            }
+            return instance;
+        }
+
+
+        public double[][] NextMatrix(int redovi, int stupci)
+        {
+            double[][] matrica = new double[redovi][];
+            for (int i = 0; i < redovi; i++)
+            {
+                matrica[i] = new double[stupci];
+            }
+            for (int i = 0; i < redovi; i++)
+            {
+                for (int j = 0; j < redovi; j++)
+                {
+                    matrica[i][j] = this.generator.NextDouble();
+                }
+            }
+            return matrica;
+        }
+
+    }
+
+}
