38c694ae4b3ec0e5c0ad5de9c05d3485e087e2e2
juris-hub <65715599+juris-hub@users.noreply.github.com>
2020-05-25 20:19:34 +0200

    Add files via upload

---
+++ L3/Zad1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dataset data1 = new Dataset("csv.csv");
+            Dataset data2 = (Dataset)data1.Clone();
+
+            data1.Print();
+            data2.Change("change");
+            data2.Print();
+            data1.ClearData();
+
+            data1.Print();
+            data2.Print();
+        }
+
+
+    }
+}
