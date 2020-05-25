6fd8f6696fb50954b8ea250e937b0ef557ed44aa
juris-hub <65715599+juris-hub@users.noreply.github.com>
2020-05-25 20:02:42 +0200

    Add files via upload

---
+++ L2/Zad1/Die.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2
+{
+    class Die
+    {
+        private int numberOfSides;
+        private Random randomGenerator;
+        public Die(int numberOfSides, Random randomGenerator)
+        {
+            this.numberOfSides = numberOfSides;
+            this.randomGenerator = randomGenerator;
+        }
+        public int Roll()
+        {
+            int rolledNumber = randomGenerator.Next(1, numberOfSides + 1);
+            return rolledNumber;
+        }
+    }
+}
---
+++ L2/Zad1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Zadatak 1
+            DiceRoller kockicoVrtac = new DiceRoller();
+            
+            for(int i = 0; i < 20; i++)
+            {
+                Die kockica = new Die(6);
+                kockicoVrtac.InsertDie(kockica);
+            }
+            kockicoVrtac.RollAllDice();
+            IList<int> rezultat = kockicoVrtac.GetRollingResults();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(rezultat[i]);
+            }
+            */
+
+            //Zadatak 2
+            /*
+            DiceRoller kockicoVrtac = new DiceRoller();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Die kockica = new Die(6, new Random());
+                kockicoVrtac.InsertDie(kockica);
+            }
+            kockicoVrtac.RollAllDice();
+            IList<int> rezultat = kockicoVrtac.GetRollingResults();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(rezultat[i]);
+            }
+            */
+        }
+    }
+}
